39f170002443273a87aaf07965e68928dc468960
SHFeMIX <alanshao_sh@foxmail.com>
2025-02-16 03:34:49 +0800

    temp rules structure

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,32 @@
 import { useWebExtensionStorage } from '~/composables/useWebExtensionStorage'
 
-export const redirectRules = useWebExtensionStorage('redirectRules', [1, 2, 3])
+export const redirectRules = useWebExtensionStorage('redirectRules', [
+  {
+    id: 1,
+    active: true,
+    name: 'Google',
+    pattern: 'google.com',
+    redirect: 'https://bing.com',
+  },
+  {
+    id: 2,
+    active: true,
+    name: 'Bing',
+    pattern: 'bing.com',
+    redirect: 'https://google.com',
+  },
+  {
+    id: 3,
+    active: true,
+    name: 'Yahoo',
+    pattern: 'yahoo.com',
+    redirect: 'https://duckduckgo.com',
+  },
+  {
+    id: 4,
+    active: true,
+    name: 'DuckDuckGo',
+    pattern: 'duckduckgo.com',
+    redirect: 'https://yahoo.com',
+  },
+])
